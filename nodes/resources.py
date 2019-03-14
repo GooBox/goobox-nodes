53e05fab99a1d74ad96eadaeb2be47032b813de7
José Antonio Perdiguero López <perdy@perdy.io>
2019-03-14 16:13:39 +0100

    Update requirements

--- nodes/resources.py
+++ nodes/resources.py
@@ -17,4 +17,16 @@ class SiaNodeResource(metaclass=CRUDListDropResource):
     @database.transaction()
     @resource_method("/", methods=["POST"])
     async def create(self, element: schemas.SiaNode, generator: IDGenerator):
+        """
+            tags:
+                - Sia Node
+            summary:
+                Create a new document.
+            description:
+                Create a new document in this resource.
+            responses:
+                201:
+                    description:
+                        Document created successfully.
+        """
         return await self._create({"id": generator.generate_node_sia(element["public_key_string"]), **element})
